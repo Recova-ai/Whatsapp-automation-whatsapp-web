9428a76fbac544b59fbeeaf74d38a0c5c88df09f
Recova-ai <76469490+Recova-ai@users.noreply.github.com>
2020-12-24 13:04:18 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+import pywhatkit
+pywhatkit.sendwhatmsg('+918903426081', 'python testing messgae', 21, 56)
+
+
+
+
+
+
+
+
+
+import os
+
+a = "www.google.com" #example
+response = os.system("ping - 1 " + a)
+
+#and then check the response...
+if response == 0:
+  print(a, 'is up!')
+else:
+  print(a, 'is down!')
+
+
+
+
+
